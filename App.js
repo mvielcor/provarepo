b1532a4b1243a2957bd9a1b4b230aba35da592fd
Manel <mviel@florida-uni.es>
2021-11-02 11:17:13 +0100

    amb vector-icons

--- App.js
+++ App.js
@@ -17,20 +17,40 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {Provider as PaperProvider, Appbar} from 'react-native-paper';
 
 const App: () => Node = () => {
-  
   return (
-    <View>
-      <StatusBar barStyle='#222' />
+    <PaperProvider>
+      <Appbar style={styles.bottom}>
+        <Appbar.Action
+          icon="archive"
+          onPress={() => console.log('Pressed archive')}
+        />
+        <Appbar.Action
+          icon="mail"
+          onPress={() => console.log('Pressed mail')}
+        />
+        <Appbar.Action
+          icon="label"
+          onPress={() => console.log('Pressed label')}
+        />
+        <Appbar.Action
+          icon="delete"
+          onPress={() => console.log('Pressed delete')}
+        />
+      </Appbar>
+      <StatusBar barStyle="#222" />
       <ScrollView>
         <Text style={styles.TitolRoig}>Benvinguts a React Native</Text>
-        <Text style={styles.TitolBlau}>Este serà el primer exercici que fem.</Text>
+        <Text style={styles.TitolBlau}>
+          Este serà el primer exercici que fem.
+        </Text>
         <Text style={styles.FontNormal}>normal</Text>
         <Text style={styles.FontNotoserif}>notoserif</Text>
         <Text style={{fontFamily: 'sans-serif', fontSize: 20}}>sans-serif</Text>
       </ScrollView>
-    </View>
+    </PaperProvider>
   );
 };
 
@@ -52,7 +72,13 @@ const styles = StyleSheet.create({
   FontNotoserif: {
     fontFamily: 'notoserif',
     fontSize: 20,
-  }
+  },
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 });
 
 export default App;
